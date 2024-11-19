860c67b0436082c85e6c939c0a7c01c89164c335
tharun <tharuntk143143@gmail.com>
2024-11-19 12:45:03 +0430

    url

--- backend/server.js
+++ backend/server.js
@@ -26,3 +26,5 @@ mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
   .catch((err) => console.error("Database connection failed:", err));
+
+ //mongodb+srv://tharuntk143143:1234@navi.7n4g6.mongodb.net/
